07d0f07b03d0222d2d9500972de9275c5fb1bc02
SeungMIn <sdh220118@sdh.hs.kr>
2022-07-10 06:01:26 +0900

    GameOver

---
+++ SchoolGameJam/Assets/Script/SeungMIn/GameOver.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class End : MonoBehaviour
+{
+    Image Img;
+
+    private void Awake()
+    {
+        Img = GetComponent<Image>();
+    }
+    public void FadeInOut(float fadevalue, int time)
+    {
+        Img.DOFade(fadevalue, time);
+    }
+
+    void GameOver()
+    {
+        FadeInOut(0.7f, 1);
+        
+    }
+}
